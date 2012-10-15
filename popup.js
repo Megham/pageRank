093fee2c864c891bad9807cf84fc94447c561f5b
M.K.Ramkumar <ram.psg.cse@gmail.com>
2012-10-15 15:42:47 +0530

    ram | initial commit

---
+++ popup.js
@@ -0,0 +1,20 @@
+function loadPageRank()
+{
+	var pageRankContainer = document.getElementById('page_rank');
+	chrome.tabs.getSelected(null, function(tab) {
+		var pageRankXhr= new XMLHttpRequest();
+		var l = document.createElement("a");
+		l.href = tab.url;
+		var hostname = l.hostname;
+		var pageRankService='http://josh-fowler.com/prapi/?url='+ hostname;
+		pageRankXhr.open("GET",pageRankService,true);
+		pageRankXhr.onreadystatechange= function()
+		{
+			if(pageRankXhr.readyState==4)
+				pageRankContainer.innerHTML = "Page Rank for "+ hostname +" is " +pageRankXhr.response;
+		};
+		pageRankXhr.send();
+	});
+}
+
+document.addEventListener("DOMContentLoaded", loadPageRank, false);
