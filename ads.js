f7123c53bc654d48a9785eeb1ef72b4c20142551
Sasi kumar <sasi.madhav2005@gmail.com>
2012-10-22 20:19:19 +0530

    Blacklisted urls for page rank and ad display

--- ads.js
+++ ads.js
@@ -4,13 +4,28 @@ function injectAdd(){
 	var body_width = $("html").width();
 
 	//$("body").html("");
-	$("body").prepend("<iframe style='height:90px; width:728px; border:1px solid black;z-index: 10000;display:block;\
-position: relative;top: 0px;' src='"+adhtml+"'></iframe>");
+	$("body").prepend("<div style='width:1000px;'><iframe style='height:90px; width:728px;margin:0px auto; border:1px solid black;z-index: 10000;display:block;\
+position: relative;top: 0px;' src='"+adhtml+"'></iframe></div>");
 	//$("body").append("<iframe style='height:"+body_height+"px;width:"+body_width+"px' src='"+window.location+"'></iframe>");
-	$("body").append("<iframe style='height:90px; width:728px; border:1px solid black;z-index: 10000;\
+	$("body").append("<div style='width:1000px;'><iframe style='height:90px; width:728px; margin: 0px auto; border:1px solid black;z-index: 10000;\
 position: relative; display:block;\
-bottom: 0px;' src='"+adhtml+"'></iframe>");
+bottom: 0px;' src='"+adhtml+"'></iframe></div>");
 
 };
 
-document.body.onload = injectAdd;
+function checkBlackListedUrl(Url){
+	var blackListedUrls = ["\\.google\\.","www\\.youtube\\.com","doubleclick\\.net","facebook\\.com"];
+	for( x in blackListedUrls ){
+		var patt = new RegExp(blackListedUrls[x]);
+		if(patt.test(Url)){
+			return true;
+		}
+	}
+	return false;
+}
+
+$(document).ready(function(){
+	if(!checkBlackListedUrl(document.location.hostname)){
+		injectAdd();
+	}
+})
--- background.js
+++ background.js
@@ -10,10 +10,21 @@ chrome.tabs.onHighlighted.addListener(function(highlightInfo) {
 	updatePageRank(tabToUrl[highlightInfo.tabIds[0]]);
 })
 
+function checkPageRankBlackListedUrl(Url){
+	var blackListedUrls = ["newtab","chrome://"];
+	for(x in blackListedUrls){
+		var patt = new RegExp(blackListedUrls[x]);
+		if(patt.test(Url)){
+			return true;
+		}
+	}
+	return false;
+}
+
 function updatePageRank(tabUrl)
 {
 	chrome.browserAction.setBadgeText({text: ""});
-	if(tabUrl != "newtab")
+	if(tabUrl != undefined && !checkPageRankBlackListedUrl(tabUrl))
 	{
 		var pageRankXhr= new XMLHttpRequest();
 		var pageRankService='http://josh-fowler.com/prapi/?url='+ tabUrl;
