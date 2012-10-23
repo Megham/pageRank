c3ad4e55f1e2f917a5051f8d0318d3790d21a805
M.K.Ramkumar <ram.psg.cse@gmail.com>
2012-10-23 19:23:00 +0530

    ram | displaying add in the middle

--- ads.js
+++ ads.js
@@ -4,12 +4,12 @@ function injectAdd(){
 	var body_width = $("html").width();
 
 	//$("body").html("");
-	$("body").prepend("<div style='width:1000px;'><iframe style='height:90px; width:728px;margin:0px auto; border:1px solid black;z-index: 10000;display:block;\
+	$("body").prepend("<div style='width:"+body_width+"px;'><iframe style='height:90px; width:728px;margin:0px auto; border:1px solid black;z-index: 10000;display:block;\
 position: relative;top: 0px;' src='"+adhtml+"'></iframe></div>");
 	//$("body").append("<iframe style='height:"+body_height+"px;width:"+body_width+"px' src='"+window.location+"'></iframe>");
-	$("body").append("<div style='width:1000px;'><iframe style='height:90px; width:728px; margin: 0px auto; border:1px solid black;z-index: 10000;\
-position: relative; display:block;\
-bottom: 0px;' src='"+adhtml+"'></iframe></div>");
+//	$("body").append("<div style='width:1000px;'><iframe style='height:90px; width:728px; margin: 0px auto; border:1px solid black;z-index: 10000;\
+//position: relative; display:block;\
+//bottom: 0px;' src='"+adhtml+"'></iframe></div>");
 
 };
 
