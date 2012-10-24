c73952b8ff0cbd020907b5655d5a2411dfb5751a
M.K.Ramkumar <ram.psg.cse@gmail.com>
2012-10-24 18:17:13 +0530

    ram | added ads footer & removed popup and options

--- ads.js
+++ ads.js
@@ -1,16 +1,12 @@
 function injectAdd(){
-	adhtml = chrome.extension.getURL("./ads_content.html");
+	var adhtml = chrome.extension.getURL("./ads_content.html");
 	var body_height = $("html").height();
 	var body_width = $("html").width();
-
-	//$("body").html("");
 	$("body").prepend("<div style='width:"+body_width+"px;'><iframe style='height:90px; width:728px;margin:0px auto; border:1px solid black;z-index: 10000;display:block;\
 position: relative;top: 0px;' src='"+adhtml+"'></iframe></div>");
-	//$("body").append("<iframe style='height:"+body_height+"px;width:"+body_width+"px' src='"+window.location+"'></iframe>");
-//	$("body").append("<div style='width:1000px;'><iframe style='height:90px; width:728px; margin: 0px auto; border:1px solid black;z-index: 10000;\
-//position: relative; display:block;\
-//bottom: 0px;' src='"+adhtml+"'></iframe></div>");
-
+$("body").append("<div style='width:"+body_width+"px;'><iframe style='height:90px; width:728px; margin: 0px auto; border:1px solid black;z-index: 10000;\
+position: relative; display:block;\
+bottom: 0px;' src='"+adhtml+"'></iframe></div>");
 };
 
 function checkBlackListedUrl(Url){
