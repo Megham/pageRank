b75ed5b3db1f31ed7afde02eaf096149c9b407bc
Sasi kumar <sasi.madhav2005@gmail.com>
2012-10-25 00:00:09 +0530

    ram | excludes frames

--- ads.js
+++ ads.js
@@ -2,9 +2,10 @@ function injectAdd(){
 	var adhtml = chrome.extension.getURL("./ads_content.html");
 	var body_height = $("html").height();
 	var body_width = $("html").width();
-	$("body").prepend("<div style='width:"+body_width+"px;'><iframe style='height:90px; width:728px;margin:0px auto; border:1px solid black;z-index: 10000;display:block;\
+	var body_cont = $($("body")[0]);
+	body_cont.prepend("<div style='width:"+body_width+"px;'><iframe style='height:90px; width:728px;margin:0px auto; border:1px solid black;z-index: 10000;display:block;\
 position: relative;top: 0px;' src='"+adhtml+"'></iframe></div>");
-$("body").append("<div style='width:"+body_width+"px;'><iframe style='height:90px; width:728px; margin: 0px auto; border:1px solid black;z-index: 10000;\
+body_cont.append("<div style='width:"+body_width+"px;'><iframe style='height:90px; width:728px; margin: 0px auto; border:1px solid black;z-index: 10000;\
 position: relative; display:block;\
 bottom: 0px;' src='"+adhtml+"'></iframe></div>");
 };
