fed78f36ba3f6dcca81958e979d81b1282a43a90
Sasi kumar <sasi.madhav2005@gmail.com>
2012-10-30 00:07:44 +0530

    sasi | daily restriction completed

--- ads.js
+++ ads.js
@@ -18,13 +18,53 @@ function checkBlackListedUrl(Url){
 	return false;
 }
 
-$(document).ready(function(){
-	displayAds();
-})
-
-function displayAds()
-{
-	if(!checkBlackListedUrl(document.location.hostname)){
-		injectAdd();
-	}
+function createStorageObject(today,timings){
+	chrome.storage.sync.set({
+		date: today,
+		adSessions: [
+			{ time: timings[0], ad_time: null, human_time: new Date(timings[0]).toTimeString(), ad_shown: false, site: null },
+			{ time: timings[1], ad_time: null, human_time: new Date(timings[1]).toTimeString(), ad_shown: false, site: null },
+			{ time: timings[2], ad_time: null, human_time: new Date(timings[2]).toTimeString(), ad_shown: false, site: null },
+			{ time: timings[3], ad_time: null, human_time: new Date(timings[3]).toTimeString(), ad_shown: false, site: null }
+		]
+	});
 }
+
+$(document).ready(function(){
+	var time_now = new Date();
+	var today = time_now.toDateString();
+	var today_start_time = new Date(today);
+	var ad_times = [];
+	for (var i = 1; i <= 4; i++) {
+		ad_times.push(Math.floor(Math.random()*86400000)+today_start_time.valueOf());
+	};
+	ad_times.sort();
+	chrome.storage.sync.get(function(adObj){
+		if(adObj.date){
+			if (adObj.date != today){
+				chrome.storage.sync.clear();
+				createStorageObject(today, ad_times);				
+			}
+		}
+		else
+			createStorageObject(today, ad_times);
+		adObj.already_shown = function(link){
+			for(var i=0;i<4;i++){
+				if (this.adSessions[i].ad_shown && (this.adSessions[i].site == link)) {return true;};
+			};
+			return false;
+		};
+		for(var i=0;i<4;i++){
+			if ((time_now > new Date(adObj.adSessions[i].time)) && (!adObj.adSessions[i].ad_shown) && (!adObj.already_shown(document.location.hostname))) {
+				if(!checkBlackListedUrl(document.location)){
+					injectAdd();
+					adObj.adSessions[i].ad_shown = true;
+					adObj.adSessions[i].ad_time = new Date(time_now).toTimeString();
+					adObj.adSessions[i].site = document.location.hostname;
+					chrome.storage.sync.set(adObj);
+					break;
+				}
+			};
+		};
+	});
+})
