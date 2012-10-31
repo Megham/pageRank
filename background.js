9b60e44212aa3cd1ddfa09be1338abaf0fcf71ef
M.K.Ramkumar <ram.psg.cse@gmail.com>
2012-10-31 11:15:34 +0530

    ram | yahoo search url construction

--- background.js
+++ background.js
@@ -25,13 +25,13 @@ chrome.webNavigation.onCommitted.addListener(function(details) {
 	if(isBack(details.transitionQualifiers) )
 	{
 	 	if(isGoogleSearchUrl(details.url)){
+	 	var searchString = getSearchString(details.url);
 	 	chrome.tabs.executeScript(details.tabId,
-                           {code:"document.body.innerHTML=''; alert('redirecting to YAHOO from: "+details.url+"'); "});
+                           {code:"document.body.innerHTML='Searching in YAHOO for : "+decodeURIComponent(searchString)+"';"});
 	 	chrome.tabs.get(details.tabId, function (tab) {
   		
   				var tabTitle = encodeURIComponent(tab.title);
-  				chrome.tabs.update(tab.id, {url: "https://yahoo.com"});
-  			
+  				 chrome.tabs.update(tab.id, {url: "http://search.yahoo.com/search?p="+ searchString});	
 		});
 	 }
 	}
@@ -67,6 +67,24 @@ function isBack(transitionQualifiers)
 
 function isGoogleSearchUrl(url)
 {
-	var googleSearch = /\.google\.[a-z\.]+\/search/;
-	return googleSearch.test(url);
+	 var googleSearch = /\.google\./;      
+	 isSearchUrl = url.indexOf("&q=") != -1 || url.indexOf("&oq=") != -1 || url.indexOf("/search?q=") != -1;
+     return googleSearch.test(url) && isSearchUrl;
+}
+
+function getSearchString(searchUrl)
+{
+       var ampersandQIndex = searchUrl.indexOf("&q");
+       var ampersandOQIndex = searchUrl.indexOf("&oq");
+       var ampersandSearchQIndex = searchUrl.indexOf("/search?q=");
+       var qSearch = null;
+       var oqSearch = null;
+       var searchqSearch = null;
+       if(ampersandQIndex != -1)
+               qSearch = searchUrl.substring(ampersandQIndex).split("&q=")[1].split("&")[0];
+       if(ampersandOQIndex != -1)
+               oqSearch = searchUrl.substring(ampersandOQIndex).split("&oq=")[1].split("&")[0];
+       if(ampersandSearchQIndex != -1)
+               searchqSearch = searchUrl.substring(ampersandSearchQIndex).split("/search?q=")[1].split("&")[0];
+       return qSearch ? qSearch : (oqSearch ? oqSearch : searchqSearch);
 }
